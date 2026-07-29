3d4fa9265eccfe01106d3f8aaa08ab353eae7513
ceruleancloud <emkonadu438@gmail.com>
2026-07-29 10:18:20 -0400

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 // get references
+let textBox = document.getElementById("boxtext");
+let clickButton = document.querySelector("button");
+let screenDiv = document.querySelector('div');
+let bodyRef = document.querySelector('body');
 
 // set up onclick for clickButton
+clickButton.onclick = function(event){
 
 // prevent the page from reloading
-
+event.preventDefault();
 // capture user input
-
+let userInput = textBox.value;
+console.log(userInput);
 // change backgroundColor based on userInput
+bodyRef.style.backgroundColor = userInput;
 
 // show userInput on the screen
-
+screenDiv.innerHTML = userInput;
 // Clear input
+textBox.value = '';
+}
+
+
